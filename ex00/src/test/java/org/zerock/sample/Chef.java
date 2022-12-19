2f7fff2db669f2a030021b9d225a796db67b3dab
pingsing <piosio990@gmail.com>
2022-12-19 18:13:56 +0900

    2022-12-19 18:13

---
+++ ex00/src/test/java/org/zerock/sample/Chef.java
@@ -0,0 +1,11 @@
+package org.zerock.sample;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component	// 해당 클래스가 스프링에서 객체로 만들어서 관리하는 대상임을 명시하는 어노테이션
+@Data
+public class Chef {
+	
+}
